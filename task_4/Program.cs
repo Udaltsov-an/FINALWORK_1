b2f7b8cb5d62aa6556b1c4d4f703919c2ca038ce
Aleksandr Udaltsov <aln.udaltsov@yandex.ru>
2023-01-24 12:13:49 +0300

    Решение задачи 4

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] FillArray(int length)
+{
+    string[] array = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+       Console.Write($"Введите {i+1} значение массива: ");
+       array[i] = Console.ReadLine();
+       Console.WriteLine();
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int length = int.Parse(Console.ReadLine());
+    return length;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+Console.Clear();
+int length = EnterData("Введите количество элементов массива: ");
+string[] array = FillArray(length);
+string[] result_array = new string[array.Length];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result_array[count] = array[i];
+        count++;
+    }
+}
+PrintArray(result_array);
